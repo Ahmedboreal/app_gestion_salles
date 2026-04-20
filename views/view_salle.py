b7419531c8d467711286299b2c11a748237131eb
ahmed <ahmed@email.com>
2026-04-20 03:43:06 -0400

    Création de la classe ViewSalle héritant de ctk.CTk avec l"attribut service_salle

---
+++ views/view_salle.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+from services.services_salle import ServiceSalle
+
+
+class ViewSalle(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.service_salle = ServiceSalle()
+
+        self.title("Gestion des salles")
+        self.geometry("700x500")
