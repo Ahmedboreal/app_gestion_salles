75be5c64e933d15ed770fb937e18a83b184db243
ahmed <ahmed@email.com>
2026-04-20 00:20:10 -0400

    Création de la methode get_connection pour lire config.json et se connecter a MySQL

---
+++ data/dao_salle.py
@@ -0,0 +1,17 @@
+import json
+import mysql.connector
+
+
+class DataSalle:
+    def get_connection(self):
+        with open("data/config.json", "r") as fichier:
+            config = json.load(fichier)
+
+        connexion = mysql.connector.connect(
+            host=config["host"],
+            user=config["user"],
+            password=config["password"],
+            database=config["database"]
+        )
+
+        return connexion
