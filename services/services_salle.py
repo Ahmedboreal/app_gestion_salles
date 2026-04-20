93f380e2f6406a93e7c5f85139713fc11ee6119c
ahmed <ahmed@email.com>
2026-04-20 02:37:15 -0400

    ajout de la méthode ajouter_salle avec validation des données

--- services/services_salle.py
+++ services/services_salle.py
@@ -4,3 +4,13 @@ from models.salle import Salle
 class ServiceSalle:
     def __init__(self):
         self.dao_salle = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if not salle.code or not salle.libelle or not salle.type or not salle.capacite:
+            return False, " Toutes les données de la salle doivent etre renseignées."
+
+        if salle.capacite < 1:
+            return False, "La capacité doit etre supérieure ou égale a 1."
+
+        self.dao_salle.insert_salle(salle)
+        return True, " salle ajoutée avec succés."
